40e225f5119b8fb6433747b73c2c3b0ad27d09fd
Manh Nguyen <nguyengmanh@gmail.com>
2020-02-10 14:07:31 -0800

    ch2exercises

---
+++ chapters/2_variables/name.rb
@@ -0,0 +1,17 @@
+#exercise one#
+puts "Hey! Type in your name!"
+text = gets.chomp
+puts "Hello! #{text}"
+
+#exercise three#
+10.times {puts text}
+
+#exercise four#
+puts "What's your first name?"
+first = gets.chomp
+puts "What's your last name?"
+last = gets.chomp
+puts "#{first} #{last}"
+
+#exercise five#
+#the first prints 3 and the second gives an error because x is created in the block#
