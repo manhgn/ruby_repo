52f99a54fb81c5aa40524618d22175c02fdf705b
Manh Nguyen <nguyengmanh@gmail.com>
2020-02-18 17:51:30 -0800

    ch1exercises

--- chapters/1_thebasics/ch1exercises.rb
+++ chapters/1_thebasics/ch1exercises.rb
@@ -6,3 +6,33 @@ x/1000
 x%1000 / 100
 x%100 / 10
 x%10
+
+#exercise three#
+movies = { :uncut_gems => 2019,
+:toy_story => 1995,
+:shrek => 2001}
+puts movies[:uncut_gems]
+puts movies[:toy_story]
+puts movies[:shrek]
+
+#exercise four#
+dates = [1975, 2004, 2013, 2001, 1981]
+puts dates[0]
+puts dates[1]
+puts dates[2]
+puts dates[3]
+puts dates[4]
+
+#exercise five#
+puts 5 * 4 * 3 * 2 * 1
+puts 6 * 5 * 4 * 3 * 2 * 1
+puts 7 * 6 * 5 * 4 * 3 * 2 * 1
+puts 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1
+
+#exercise six#
+puts 2.234**2
+puts 43.19**2
+puts 234.54**2
+
+#exercise seven#
+#there is a missing closing  
